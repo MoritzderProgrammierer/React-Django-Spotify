50e95d8c5076d57649bde898eed8c6af60f3b908
MoritzderProgrammierer <moritzlang97@web.de>
2021-02-10 08:50:50 +0100

    inital

---
+++ music_controller/spotify/credentials.py
@@ -0,0 +1,4 @@
+CLIENT_ID = "3aa46c1f46b14a9c821ec3aae7eb6fa0"
+CLIENT_SECRET = "81dd42557a584f46952487b6ee4c253a"
+#REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+REDIRECT_URI = "http://192.168.178.29:8000/spotify/redirect"
